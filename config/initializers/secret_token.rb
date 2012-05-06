10923238189210db40110d6024ced000498f53da
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-05-06 10:38:04 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaypointManager::Application.config.secret_token = 'f793a6d52b51a045f285c1c61b0603086113cc6098f740f8dbcfb4cb7e3f24cadfbdcb827cdbfd5d844a751e0a76b15895cbef7c915437859ba6d23b7caf94f5'
