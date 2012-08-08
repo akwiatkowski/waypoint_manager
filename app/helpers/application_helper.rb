f546eba577defbf641e3c16a1858a2e0a05158a3
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-08-08 14:10:11 +0200

    waypoint map links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def waypoint_map_link_to(waypoint)
+    s = link_to "google", waypoint.google_maps_path
+    s += " "
+    s += link_to "ump", waypoint.ump_path
+
+    return s
+  end
 end
