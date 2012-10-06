08f6c73cac939d28080adbc8c2949c39ef193efe
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-10-06 17:22:37 +0200

    minor fix

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,5 +1,5 @@
 class AreasController < InheritedResources::Base
   load_and_authorize_resource
   has_scope :page, default: 1, if: Proc.new{ |r| r.request.format == 'html'}
-  has_scope :sorted, as: :sort
+  has_scope :sorted, as: :sort, default: 'name_asc'
 end
